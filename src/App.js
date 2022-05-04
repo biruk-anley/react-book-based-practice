1681e4ab8b8ff47df376cff0c67ee1b3e4dd0923
biruk <birukanley2022@gmail.com>
2022-05-04 21:43:03 +0300

    upto40

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-function App() {
+
+const lists = [
+  {
+    title: "React",
+    Url: "https://reactjs.org",
+    author: "Jordan Michel",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    Url: "https://reactreduxjs.org",
+    author: "Dab Abramov",
+    num_comments: 8,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: "Rollex",
+    Url: "https://rollexjs.org",
+    author: "David Michel",
+    num_comments: 13,
+    points: 40,
+    objectID: 2,
+  },
+  {
+    title: "Flutter",
+    Url: "https://Flutterctjs.org",
+    author: "Jordan domMichel",
+    num_comments: 3,
+    points: 4,
+    objectID: 3,
+  },
+  {
+    title: "Python",
+    Url: "https://pythonjs.org",
+    author: "Michel peterson",
+    num_comments: 3,
+    points: 7,
+    objectID: 4,
+  },
+];
+
+const List = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {lists.map((item) => {
+        return (
+          <div className="body">
+            <li key={item.objectID}>
+              <h4>{item.title}</h4>
+              <p>
+                author: {item.author} comments :<span>{item.points}</span>
+              </p>
+              <p>
+                tutorial link : <a href={item.Url}>{item.Url}</a>
+              </p>
+              <hr />
+            </li>
+          </div>
+        );
+      })}
+    </ul>
+  );
+};
+
+const Search = () => {
+  return (
+    <div className="search">
+      <label htmlFor="search">Name</label>
+      <input type="text" id="search" />
     </div>
   );
 }
+
+const App = () => {
+  
+  return (
+    <div className="holder">
+      <h1>Practising on lists using map function</h1>
+      <Search/>
+      <hr />
+      <List/>
+
+      
+    </div>
+  );
+};
 
 export default App;
