d32b52bf179afc7a965ea5ceee0bbd213ba9d4bb
biruk <birukanley2022@gmail.com>
2022-05-07 11:16:12 +0300

    updating app component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,92 @@
 import * as React from "react";
+
 import "./App.css";
 
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      Url: "https://reactjs.org",
+      author: "Jordan Michel",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      Url: "https://reactreduxjs.org",
+      author: "Dab Abramov",
+      num_comments: 8,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: "Rollex",
+      Url: "https://rollexjs.org",
+      author: "David Michel",
+      num_comments: 13,
+      points: 40,
+      objectID: 2,
+    },
+    {
+      title: "Flutter",
+      Url: "https://Flutterctjs.org",
+      author: "Jordan domMichel",
+      num_comments: 3,
+      points: 4,
+      objectID: 3,
+    },
+    {
+      title: "Python",
+      Url: "https://pythonjs.org",
+      author: "Michel peterson",
+      num_comments: 3,
+      points: 7,
+      objectID: 4,
+    },
+  ];
+  const [searchTerm, setSearchTerm] = React.useState("");
+ 
+  //  A
+  const handleSearch = (event) => {
+     setSearchTerm(event.target.value);
+  };
 
-const lists = [
-  {
-    title: "React",
-    Url: "https://reactjs.org",
-    author: "Jordan Michel",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    Url: "https://reactreduxjs.org",
-    author: "Dab Abramov",
-    num_comments: 8,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: "Rollex",
-    Url: "https://rollexjs.org",
-    author: "David Michel",
-    num_comments: 13,
-    points: 40,
-    objectID: 2,
-  },
-  {
-    title: "Flutter",
-    Url: "https://Flutterctjs.org",
-    author: "Jordan domMichel",
-    num_comments: 3,
-    points: 4,
-    objectID: 3,
-  },
-  {
-    title: "Python",
-    Url: "https://pythonjs.org",
-    author: "Michel peterson",
-    num_comments: 3,
-    points: 7,
-    objectID: 4,
-  },
-];
+  const searchedStories = stories.filter((story) => {
+    return (story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+  });
 
-const List = () => {
+  return (
+    <div className="holder">
+      <h1>Practising on List using map function</h1>
+      {/* //B  handle search emibal function alen.. on search ... handle searchin pass yadergal... */}
+      <Search onSearch={handleSearch} />
+
+      <hr />
+      <List listname={searchedStories} />
+    </div>
+  );
+};
+
+const Search = (props) => {
+  
+
+  return (
+    <div className="search">
+      <label htmlFor="search">Name</label>
+      <input type="text" id="search" onChange={props.onSearch} />
+     
+    </div>
+  );
+};
+
+const List = (props) => {
   return (
     <ul>
-      {lists.map((item) => {
+      {props.listname.map((item) => {
         return (
           <div className="body">
-            <li key={item.objectID}>
-              <h4>{item.title}</h4>
-              <p>
-                author: {item.author} comments :<span>{item.points}</span>
-              </p>
-              <p>
-                tutorial link : <a href={item.Url}>{item.Url}</a>
-              </p>
-              <hr />
-            </li>
+            <Item key={item.objectID} item={item} />
           </div>
         );
       })}
@@ -68,26 +94,18 @@ const List = () => {
   );
 };
 
-const Search = () => {
+const Item = (props) => {
   return (
-    <div className="search">
-      <label htmlFor="search">Name</label>
-      <input type="text" id="search" />
-    </div>
-  );
-}
-
-const App = () => {
-  
-  return (
-    <div className="holder">
-      <h1>Practising on lists using map function</h1>
-      <Search/>
+    <li>
+      <h4>{props.item.title}</h4>
+      <p>
+        author: {props.item.author} comments :<span>{props.item.points}</span>
+      </p>
+      <p>
+        tutorial link : <a href={props.item.Url}>{props.item.Url}</a>
+      </p>
       <hr />
-      <List/>
-
-      
-    </div>
+    </li>
   );
 };
 
